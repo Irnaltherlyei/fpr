d50cb66c39d1fca51004b5373cd278561e834ccd
Adrian Kaminski <adrian.kaminski@studmail.w-hs.de>
2021-12-14 00:07:55 +0100

    Inital commit for fpr-8.1

--- src/App.java
+++ src/App.java
@@ -1,39 +1,63 @@
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.TreeSet;
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
 public class App {
+
     public static void main(String[] args) {
-        Path source = Paths.get("src/book.txt");
-        Scanner in = null;
-        try {
-            in = new Scanner(source);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        ArrayList<String> list = new ArrayList<String>();
-        // Tree Set will automatically be sorted when inserting new values as well as having only distinct values
-        TreeSet<String> set = new TreeSet<String>();
-        while(in.hasNext()){
-            String[] words = in.next()
-                    .toLowerCase()
-                    // Replaces all characters except the ones stated below.
-                    .replaceAll("[^a-zA-Z0-9']+", " ")
-                    // Replaces multiple white spaces with just one white space.
-                    .replaceAll("\\s+", " ")
-                    .trim().split(" ");
-            for (String s :
-                    words) {
-                if(s.length() > 0) {
-                    list.add(s);
-                    set.add(s);
+        new App();
+    }
+
+    JFrame frame;
+    JPanel panel;
+    JMenuBar menuBar;
+    JMenu options, help, refresh, close;
+
+    public App(){
+        frame = new JFrame();
+        panel = new JPanel();
+
+        menuBar = new JMenuBar();
+
+        options = new JMenu("Optionen");
+        options.addMenuListener(new Listener());
+        menuBar.add(options);
+        help = new JMenu("Hilfe");
+        help.addMenuListener(new Listener());
+        menuBar.add(help);
+        refresh = new JMenu("Aktualisieren");
+        refresh.addMenuListener(new Listener());
+        menuBar.add(refresh);
+        close = new JMenu("Beenden");
+        close.addMenuListener(new Listener());
+        menuBar.add(close);
+
+        frame.setJMenuBar(menuBar);
+        frame.add(panel);
+
+        frame.setVisible(true);
+        frame.setSize(720,480);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public class Listener implements MenuListener{
+
+        @Override
+        public void menuSelected(MenuEvent e) {
+            if(e.getSource() == close){
+                int clicked = JOptionPane.showConfirmDialog(frame,"Are you sure you want to quit?");
+                if(clicked == 0){
+                    frame.dispose();
                 }
             }
         }
-        System.out.println(list.size());
-        System.out.println(set.size());
+
+        @Override
+        public void menuDeselected(MenuEvent e) {
+        }
+
+        @Override
+        public void menuCanceled(MenuEvent e) {
+        }
     }
 }
